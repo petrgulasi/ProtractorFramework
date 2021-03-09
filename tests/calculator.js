4934a751c39eab88aff0b6e5092bcf0b8aa5d106
PGU <petr.gulasi@live.com>
2021-03-09 22:45:42 +0100

    My first commit again

--- tests/calculator.js
+++ tests/calculator.js
@@ -13,4 +13,16 @@ describe('Demo calculator tests', function () {
         homepage.verifyResult('7');
         homepage.browserSleep(3000);
     });
+    it('Subtraction test', function () {
+        homepage.get('https://juliemr.github.io/protractor-demo/');
+        homepage.browserSleep(1000);
+        homepage.enterFirstNumber('4');
+        homepage.browserSleep(1000);
+        homepage.enterSecondNumber('3');
+        homepage.browserSleep(1000);
+        homepage.clickGo1();
+        homepage.browserSleep(1000);
+        homepage.verifyResult('7');
+        homepage.browserSleep(3000);
+    });
 });
